4ad35e7eca8f61711e1584cfd511ba93743cd0ed
Jeri Atkins <jeri.atkins@student.swic.edu>
2019-09-20 22:56:44 -0500

    Add numberSummer fxn

--- scripts.js
+++ scripts.js
@@ -1,8 +1,15 @@
-//Write a program that sums the numbers from 1 to 10.
-let sum = 0;
+//Write a program that sums the numbers from any given starting point and any given ending point.
+//A function can accept some named parameters
+function numberSummer(start = 1, end = 10) {
+  let sum = 0;
 
-for (let i = 1; i <= 10; i += 1) {
-  sum += i;
+  for (let i = start; i <= end; i += 1) {
+    sum += i;
+  }
+
+  return sum;
 }
 
-console.log(sum);
+// We invoke a function and pass in arguments
+const total = numberSummer(2, 100);
+console.log(total);
