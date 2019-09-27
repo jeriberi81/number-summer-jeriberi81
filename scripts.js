3707620dd3f5411991e2c6af577ee5ad228a653d
Jeri Atkins <jeri.atkins@student.swic.edu>
2019-09-27 14:26:51 -0500

    Add interval to function

--- scripts.js
+++ scripts.js
@@ -1,15 +1,15 @@
 //Write a program that sums the numbers from any given starting point and any given ending point.
 //A function can accept some named parameters
-function numberSummer(start = 1, end = 10) {
+function numberSummer(start = 1, end = 10, interval = 2) {
   let sum = 0;
 
   for (let i = start; i <= end; i += 1) {
-    sum += i;
+    sum += interval;
   }
 
   return sum;
 }
 
 // We invoke a function and pass in arguments
-const total = numberSummer(2, 100);
+const total = numberSummer(2, 100, 2);
 console.log(total);
